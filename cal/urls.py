00fb58226e538babb7cbbccd1ba9642d195e44cb
kul96 <kuldeepsuthar96@gmail.com>
2020-02-17 14:33:52 +0530

    update urls

--- cal/urls.py
+++ cal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('base/',views.base),
-    path('add/',views.add)
-]
+    path('base',views.base),
+    path('add',views.add),
+    path('cal1_1',views.cal1_1)  
+] 
