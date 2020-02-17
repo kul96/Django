106881bd7693999624f3822c2cb666a852e2edd7
kul96 <kuldeepsuthar96@gmail.com>
2020-02-17 14:34:27 +0530

    add view cal1_1

--- cal/views.py
+++ cal/views.py
@@ -5,15 +5,17 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html',{'name':'kuldeep'})
-    #return HttpResponse("hello world")
-
+    # return HttpResponse("hello world")
+ 
 def base(request):
     return render(request,'base.html')
 
 def add(request):
 
-    n1 = int(request.GET['num1'])
-    n2 = int(request.GET['num2'])
+    n1 = int(request.POST['num1'])
+    n2 = int(request.POST['num2'])
     res = n1+n2  
     return render(request,'result.html',{'res':res})
 
+def cal1_1(request):
+    return render(request,'cal1_1.html')
